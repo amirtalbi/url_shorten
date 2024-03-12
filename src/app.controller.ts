223bc799a89935d0c203acf24624b56459aac48e
Amir Talbi <amirotalbi94@gmail.com>
2024-03-12 12:05:36 +0100

    feat: adding dto for url

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,26 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject, Param, Post, Body } from '@nestjs/common';
 import { AppService } from './app.service';
+import { DynamicService } from './services/dynamic.service';
+import { StaticService } from './services/static.service';
+import { UrlDto } from './dto/url.dto';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    @Inject(DynamicService) private readonly dynamicService: DynamicService,
+    @Inject(StaticService) private readonly staticService: StaticService,
+  ) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('/:url')
+  getUrl(@Param('url') url: string): string {
+    console.log(url);
+    return this.staticService.getUrl(url);
+  }
+
+  @Post('/create-url')
+  createUrl(@Body() body: UrlDto): string {
+    console.log(body);
+    return this.dynamicService.createUrl(body.url);
   }
 }
